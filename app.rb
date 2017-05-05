f9a45accb0d3e8247609e7d1e8b737f214172501
david gray <davidseangray@gmail.com>
2017-05-05 11:27:17 +0100

    added sinatra reloader

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class BookmarkManager < Sinatra::Base
   enable :sessions
 
   get '/' do
-
+    redirect '/links'
   end
 
   get '/links' do
@@ -22,8 +22,9 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.create(url: params[:url], title: params[:title])
-    tag = Tag.first_or_create(name: params[:tags])
-    link.tags << tag
+     params[:tags].split.each do |tag|
+       link.tags << Tag.first_or_create(name: tag)
+    end
     link.save
     redirect '/links'
   end
--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -11,4 +11,14 @@ feature 'Adding tags' do
     expect(link.tags.map(&:name)).to include('education')
   end
 
+  scenario 'adding multiple taggs to a new link' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags',  with: 'education ruby'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
+  end
+
 end
